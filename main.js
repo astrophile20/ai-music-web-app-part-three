4c253587f8b9b8d9e4214f3b22bc066469e764a2
Ankita Ganguly <87467098+astrophile20@users.noreply.github.com>
2022-01-15 17:07:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload() {
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X: " + leftWristX);
+        console.log("Left Wrist Y: " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X: " + rightWristX);
+        console.log("Right Wrist Y: " + rightWristY);
+    }
+}
